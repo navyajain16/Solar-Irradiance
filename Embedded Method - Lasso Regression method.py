8440c819109a2112c97da26eb284a1b294baed9a
navyajain16 <72208843+navyajain16@users.noreply.github.com>
2022-01-17 21:38:08 +0530

    Update and rename embedd2.py to Embedded Method - Lasso Regression method.py

--- Embedded Method - Lasso Regression method.py
+++ Embedded Method - Lasso Regression method.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Jan 11 11:35:44 2022
-
-@author: navya
-"""
-
 import pandas as pd
 import numpy as np
 import seaborn as sns
@@ -18,30 +11,30 @@ from sklearn import metrics
 from sklearn.datasets import load_boston
 
 
-df = pd.read_csv('SolarData2019.csv')
-X = df.drop("GHI",1)   #Feature Matrix
-y = df["GHI"]    
-              # target feature
+df = pd.read_csv('C:/solar/SolarData2019.csv') #Import file
+X = df.drop("GHI",1) #Feature Matrix
+y = df["GHI"]  # target feature
 features = X.columns
-features
+features  #Printing features
 
-# Lets apply the Lasso Model -- in Linear Dataset
+# Lasso Model in Linear Dataset
 lasso = Lasso(alpha=0.05)
 lasso.fit(X,y)
 
+# Printing coefficients of Lasso regularization
 coeff = lasso.coef_
 coeff
 
+# Creating a dataframe of features with coefficient
 df_coeff = pd.DataFrame({"features":features,"coeff":coeff})
 df_coeff.sort_values("coeff")
 
-# Use Bar chart to show coeff
+# Use Bar chart to show coefficient
 df_coeff.set_index('coeff')
 # sort in ascending order to better visualization.
 df_coeff = df_coeff.sort_values('coeff')
 # plot the feature coeff in bars.
 plt.figure(figsize=(10,6))
-#plt.xticks(rotation=60)
 sns.barplot(x="features",y= "coeff", data=df_coeff,color="blue")
 plt.title("Feature importance using Lasso model",size=18)
 plt.xlabel('Feature',size=18)
@@ -50,5 +43,6 @@ plt.rc('xtick',labelsize=18)
 plt.rc('ytick',labelsize=18)
 plt.tick_params(axis='x', labelsize=18, rotation=75)
 plt.tick_params(axis='y', labelsize=18)
+# Save Plot
 plt.savefig('embedd.pdf',bbox_inches='tight')
 plt.show()
